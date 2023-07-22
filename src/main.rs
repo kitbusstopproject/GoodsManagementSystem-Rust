7228e6883139f3cf3015116efbab970f469849df
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-07-22 19:31:12 +0900

    change: routeってディレクトリにpages入れてるのキモイから消した

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-mod route;
+use yew_router::prelude::*;
+use yew::prelude::*;
 
-use route::Main;
+mod pages;
+
+use crate::pages::{ProductDetail, ProductList};
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    ProductList,
+    #[at("/product/:id")]
+    ProductDetail{ id: u32 },
+}
+
+fn switch(route: Route) -> Html {
+    match route {
+        Route::ProductList => html! { <ProductList /> },
+        Route::ProductDetail { id } => html! {<ProductDetail id={id} />},
+    }
+}
+
+#[function_component(Main)]
+pub fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} /> // <- must be child of <BrowserRouter>
+        </BrowserRouter>
+    }
+}
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
---
+++ src/pages/mod.rs
@@ -0,0 +1,5 @@
+mod product_detail;
+mod product_list;
+
+pub use product_detail::App as ProductDetail;
+pub use product_list::App as ProductList;
