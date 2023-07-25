53d75075d3844ada471a4c707448de3a0b271040
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-07-25 10:04:39 +0900

    コメント追加

--- src/pages/product_detail.rs
+++ src/pages/product_detail.rs
@@ -1,17 +1,24 @@
+// Yewとmaterial_yewのプリリュード（事前に必要なモジュール）をインポート
 use yew::prelude::*;
 use material_yew::MatButton;
 
+// ProductDetailProps構造体の定義。商品のIDを受け取るためのプロパティ
 #[derive(Clone, Properties, PartialEq)]
 pub struct ProductDetailProps {
     pub id: u32,
 }
 
+// product_detail関数コンポーネントの定義
 #[function_component(App)]
 pub fn product_detail(props: &ProductDetailProps) -> Html {
     html! {
+        // HTMLを記述する
         <main>
+            // 見出しとして「Product Detail」と表示
             <h1>{ "Product Detail" }</h1>
+            // propsで受け取った商品IDを表示
             <h1>{ format!("Product ID: {:?}", props.id) }</h1>
+            // マテリアルデザインのボタンを表示。ラベルに「Click me!」を指定し、アイコンに「code」を指定
             <MatButton label="Click me!" icon={AttrValue::from("code")} />
         </main>
     }
