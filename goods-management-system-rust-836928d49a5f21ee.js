a1c5d5a5c7ca6edbb387cd9593e95f2e6095bb3f
Taka1304 <Taka1304@users.noreply.github.com>
2023-07-27 01:00:52 +0000

    deploy: a9cd436930ff42b1109e42cdf0f3ac97ac35dd04

--- goods-management-system-rust-836928d49a5f21ee.js
+++ goods-management-system-rust-836928d49a5f21ee.js
@@ -897,7 +897,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('goods-management-system-rust-788f3242a0813b6c_bg.wasm', import.meta.url);
+        input = new URL('goods-management-system-rust-836928d49a5f21ee_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
