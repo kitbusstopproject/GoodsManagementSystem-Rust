703ddd053adabe6102eb94d8f6df3a163733360b
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-07-29 01:14:58 +0900

    Add: 編集機能Done

--- src/pages/edit_item.rs
+++ src/pages/edit_item.rs
@@ -1,6 +1,7 @@
-use atoms::{InputForm, Modal};
+use atoms::InputForm;
 use firestore_hooks::{use_document, NotFetched};
 use firestore::{update_document, delete_document};
+use material_yew::{MatDialog, WeakComponentLink, dialog::{MatDialogAction, ActionType}};
 use model::Item;
 use yew::prelude::*;
 
@@ -17,8 +18,26 @@ pub struct EditItemProps {
     pub id: String,
 }
 
+pub struct Dialog {
+    basic_dialog_link: WeakComponentLink<MatDialog>,
+    action_dialog_link: WeakComponentLink<MatDialog>,
+    scrollable_dialog_link: WeakComponentLink<MatDialog>,
+    hide_action_dialog_link: WeakComponentLink<MatDialog>,
+    stacked_dialog_link: WeakComponentLink<MatDialog>,
+    hide_actions: bool,
+}
+
+pub enum Msg {
+    ShowBasicDialog,
+    ShowActionDialog,
+    ShowScrollableDialog,
+    ShowHideActionDialog,
+    ShowStackedDialog,
+    HideActions,
+}
+
 #[function_component(App)]
-pub fn edit_item(props: &EditItemProps) -> Html {    
+pub fn edit_item(props: &EditItemProps) -> Html {
     let item_name = use_mut_ref(|| "".to_string());
     let maker = use_mut_ref(|| "".to_string());
     let model_number = use_mut_ref(|| "".to_string());
@@ -37,42 +56,56 @@ pub fn edit_item(props: &EditItemProps) -> Html {
             return html! { <div>{ "エラーが発生しました" }</div> }
         }
         Ok(item) => {
-            let item_name_str = item_name.borrow().clone();
-            let maker_str = maker.borrow().clone();
-            let model_number_str = model_number.borrow().clone();
-            let supplier_str = supplier.borrow().clone();
+            // 初期化
+            *item_name.borrow_mut() = item.item_name.clone();
+            *maker.borrow_mut() = item.maker.clone();
+            *model_number.borrow_mut() = item.model_number.clone();
+            *supplier.borrow_mut() = item.supplier.clone();
 
-            let on_input_item_name = Callback::from(move |e: InputEvent|{
-                let input_text = e
-                    .target_dyn_into::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value();
-                *item_name.borrow_mut() = input_text.clone();
-            });
+            let on_input_item_name = {
+                let item_name = item_name.clone();
+                    Callback::from(move |e: InputEvent| {
+                    let input_text = e
+                        .target_dyn_into::<web_sys::HtmlInputElement>()
+                        .unwrap()
+                        .value();
+                    *item_name.borrow_mut() = input_text.clone();
+                })
+            };
 
-            let on_input_maker = Callback::from(move |e: InputEvent|{
-                let input_text = e
-                    .target_dyn_into::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value();
-                *maker.borrow_mut() = input_text.clone();
-            });
+            let on_input_maker = {
+                let maker = maker.clone();
+                Callback::from(move |e: InputEvent|{
+                    let input_text = e
+                        .target_dyn_into::<web_sys::HtmlInputElement>()
+                        .unwrap()
+                        .value();
+                    *maker.borrow_mut() = input_text.clone();
+                })
+            };
 
-            let on_input_model_number = Callback::from(move |e: InputEvent|{
-                let input_text = e
-                    .target_dyn_into::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value();
-                *model_number.borrow_mut() = input_text.clone();
-            });
+            let on_input_model_number = {
+                let model_number = model_number.clone();
+                Callback::from(move |e: InputEvent| {
+                    let input_text = e
+                        .target_dyn_into::<web_sys::HtmlInputElement>()
+                        .unwrap()
+                        .value();
+                    *model_number.borrow_mut() = input_text.clone();
+                })
+            };
 
-            let on_input_supplier = Callback::from(move |e: InputEvent|{
-                let input_text = e
-                    .target_dyn_into::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value();
-                *supplier.borrow_mut() = input_text.clone();
-            });
+            let on_input_supplier = { 
+                let supplier = supplier.clone();
+                Callback::from(move |e: InputEvent| {
+                    let input_text = e
+                        .target_dyn_into::<web_sys::HtmlInputElement>()
+                        .unwrap()
+                        .value();
+                    *supplier.borrow_mut() = input_text.clone();
+                })
+            };
+
             let item_id = item.id.clone();
             let onsubmit = Callback::from(move|e: SubmitEvent|{
                 e.prevent_default();
@@ -82,10 +115,10 @@ pub fn edit_item(props: &EditItemProps) -> Html {
                     registered_date: item.registered_date.clone(),
                     is_lending: item.is_lending.clone(),
                     lending_log_id: item.lending_log_id.clone(),
-                    item_name: item_name_str.clone(),
-                    maker: maker_str.clone(),
-                    model_number: model_number_str.clone(),
-                    supplier: supplier_str.clone(),
+                    item_name: item_name.borrow().clone(),
+                    maker: maker.borrow().clone(),
+                    model_number: model_number.borrow().clone(),
+                    supplier: supplier.borrow().clone(),
                 };
                 log::info!("item_data: {:?}", item_data);
                 let doc_id = item.id.clone();
@@ -114,7 +147,7 @@ pub fn edit_item(props: &EditItemProps) -> Html {
                 <div class="">
                     <h1 class="text-2xl font-bold mb-6">{ "編集" }</h1>
                     <form 
-                    // onsubmit={onsubmit}
+                    onsubmit={onsubmit}
                     >
                         <div class="flex flex-col w-100">
                             <InputForm id="item_name" label="名称" placeholder={item.item_name.clone()} oninput={on_input_item_name}/>
@@ -133,27 +166,34 @@ pub fn edit_item(props: &EditItemProps) -> Html {
                                 </button>
                                 <button 
                                     type="button"
-                                    // onclick={onclick_delete}
+                                    onclick={onclick_delete}
                                     class="rounded-md bg-white hover:bg-gray-100 border border-error text-error font-bold py-4 px-8">
                                     { "削除" }
                                 </button>
                             </div>
                         </div>
                     </form>
-                    // <Modal 
-                    //     id="modal"
-                    //     tabindex={-1}
-                    //     hidden={false}
-                    // >
-                    //     <span>{"aiueo"}</span>
-                    // </Modal>
-                //     <MatDialog
-                //     heading={AttrValue::from("以下のフィールドを更新します")} 
-                //     dialog_link={self.update_dialog_link.clone()}
-                    
-                //     >
-                //     <MatDialogAction>
-                //    </MatDialog>
+                    // Modal
+                //     <section>
+                //         <span onclick={Callback::from(|_| Msg::ShowBasicDialog)}>
+                //             <button 
+                //                 label="Basic" 
+                //                 class="bg-action hover:bg-action-hover text-white font-bold py-4 px-8 rounded-md"
+                //                 />
+                //         </span>
+                //         <MatDialog 
+                //             heading={AttrValue::from("Dialog header")} 
+                //             dialog_link={Dialog.basic_dialog_link.clone()}
+                //             >
+                //             {"Dialog body text"}
+                //             <MatDialogAction action_type={ActionType::Primary} action={AttrValue::from("ok")}>
+                //                 <button label="更新する" class="bg-action hover:bg-action-hover text-white font-bold py-4 px-8 rounded-md" />
+                //             </MatDialogAction>
+                //             <MatDialogAction action_type={ActionType::Secondary} action={AttrValue::from("cancel")}>
+                //                 <button label="やめます……" class="bg-action hover:bg-action-hover text-white font-bold py-4 px-8 rounded-md" />
+                //             </MatDialogAction>
+                //         </MatDialog>
+                //     </section>
                 </div>
             }
         }
