385e014e4a9d904c276b03e1edfd75dba88ee421
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2023-07-30 17:05:18 +0900

    Add: 削除の確認モーダルを追加

--- packages/atoms/src/modal.rs
+++ packages/atoms/src/modal.rs
@@ -1,26 +1,64 @@
-use yew::{function_component, html, Html, Children, Properties};
-
+use yew::prelude::*;
 
 
 #[derive(Properties, PartialEq)]
 pub struct ModalProps {
   pub id: String,
-  pub tabindex: i32,
-  pub hidden: bool,
+  pub show: bool,
+  pub onopen: Callback<MouseEvent>,
+  pub onclose: Callback<MouseEvent>,
   pub children: Children
 }
 
-
 #[function_component(Modal)]
 pub fn modal(props: &ModalProps) -> Html {
-  
+  let modal_ref = use_node_ref();
+  // let modal = modal_ref.cast::<web_sys::HtmlDialogElement>().unwrap();
+  // let onclose = props.onclose.clone();
+  // let onopen = {
+  //   let modal = modal.clone();
+  //   let onopen = props.onopen.clone();
+  //   Callback::from(move |e: web_sys::MouseEvent| {
+  //     log::info!("open");
+  //     modal.show();
+  //     onopen.emit(e);
+  //   })
+  // };
+  // {
+  //   let modal_ref = modal_ref.clone();
+  //   let onclose = onclose.clone();
+  //   let onopen = onopen.clone();
+  //   use_effect_with_deps(
+  //     move |modal_ref| {
+  //       let modal = modal_ref.cast::<web_sys::HtmlDialogElement>().unwrap();
+  //       let onclose = {
+  //         let modal = modal.clone();
+  //         let onclose = onclose.clone();
+  //         Callback::from(move |e: web_sys::MouseEvent| {
+  //           modal.close();
+  //           onclose.emit(e);
+  //         })
+  //       };
+
+  //       let onopen = {
+  //         let modal = modal.clone();
+  //         let onopen = onopen.clone();
+  //         Callback::from(move |e: web_sys::MouseEvent| {
+  //           log::info!("open");
+  //           modal.show();
+  //           onopen.emit(e);
+  //         })
+  //       };
+  //     },
+  //     modal_ref
+  //   );
+  // };
 
   html! {
     <div 
-      id={props.id.clone()} 
-      tabindex={props.tabindex.clone().to_string()} 
-      aria-hidden={props.hidden.clone().to_string()} 
-      class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      ref={modal_ref}
+      role="dialog"
+      class="fixed bg-gray-400 opacity-30 top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
       >
       <div class="relative w-full max-w-2xl max-h-full">
         {props.children.clone()}
--- src/pages/edit_item.rs
+++ src/pages/edit_item.rs
@@ -1,4 +1,4 @@
-use atoms::InputForm;
+use atoms::{InputForm, Modal};
 use firestore_hooks::{use_document, NotFetched};
 use firestore::{update_document, delete_document};
 use material_yew::{MatDialog, WeakComponentLink, dialog::{MatDialogAction, ActionType}};
@@ -38,6 +38,20 @@ pub enum Msg {
 
 #[function_component(App)]
 pub fn edit_item(props: &EditItemProps) -> Html {
+    let is_show = use_state(|| false);
+    let show_modal = {
+        let is_show = is_show.clone();
+        Callback::from(move|_e: MouseEvent| {
+            is_show.set(true);
+        })
+    };
+    let close_modal = {
+        let is_show = is_show.clone();
+        Callback::from(move|_e: MouseEvent| {
+            is_show.set(false);
+        })
+    };
+
     let item_name = use_mut_ref(|| "".to_string());
     let maker = use_mut_ref(|| "".to_string());
     let model_number = use_mut_ref(|| "".to_string());
@@ -107,7 +121,7 @@ pub fn edit_item(props: &EditItemProps) -> Html {
             };
 
             let item_id = item.id.clone();
-            let onsubmit = Callback::from(move|e: SubmitEvent|{
+            let onsubmit = Callback::from(move|e: MouseEvent|{
                 e.prevent_default();
                 let item_data = Item {
                     id: item.id.clone(),
@@ -132,22 +146,24 @@ pub fn edit_item(props: &EditItemProps) -> Html {
                 );
             });
 
-            let onclick_delete = Callback::from(move|e: MouseEvent|{
-                e.prevent_default();
-                let doc_id = item_id.clone();
-                delete_document::<Item>(
-                    &"".to_string(),
-                    &doc_id,
-                    || {},
-                    || {},
-                );
-            });
+            let onclick_delete = {
+                Callback::from(move|e: MouseEvent|{
+                    e.prevent_default();
+                    let doc_id = item_id.clone();
+                    delete_document::<Item>(
+                        &"".to_string(),
+                        &doc_id,
+                        || {},
+                        || {},
+                    );
+                })
+            };
 
             html! {
                 <div class="">
                     <h1 class="text-2xl font-bold mb-6">{ "編集" }</h1>
                     <form 
-                    onsubmit={onsubmit}
+                        // onsubmit={onsubmit}
                     >
                         <div class="flex flex-col w-100">
                             <InputForm id="item_name" label="名称" placeholder={item.item_name.clone()} oninput={on_input_item_name}/>
@@ -166,7 +182,7 @@ pub fn edit_item(props: &EditItemProps) -> Html {
                                 </button>
                                 <button 
                                     type="button"
-                                    onclick={onclick_delete}
+                                    onclick={show_modal.clone()}
                                     class="rounded-md bg-white hover:bg-gray-100 border border-error text-error font-bold py-4 px-8">
                                     { "削除" }
                                 </button>
@@ -174,26 +190,49 @@ pub fn edit_item(props: &EditItemProps) -> Html {
                         </div>
                     </form>
                     // Modal
-                //     <section>
-                //         <span onclick={Callback::from(|_| Msg::ShowBasicDialog)}>
-                //             <button 
-                //                 label="Basic" 
-                //                 class="bg-action hover:bg-action-hover text-white font-bold py-4 px-8 rounded-md"
-                //                 />
-                //         </span>
-                //         <MatDialog 
-                //             heading={AttrValue::from("Dialog header")} 
-                //             dialog_link={Dialog.basic_dialog_link.clone()}
-                //             >
-                //             {"Dialog body text"}
-                //             <MatDialogAction action_type={ActionType::Primary} action={AttrValue::from("ok")}>
-                //                 <button label="更新する" class="bg-action hover:bg-action-hover text-white font-bold py-4 px-8 rounded-md" />
-                //             </MatDialogAction>
-                //             <MatDialogAction action_type={ActionType::Secondary} action={AttrValue::from("cancel")}>
-                //                 <button label="やめます……" class="bg-action hover:bg-action-hover text-white font-bold py-4 px-8 rounded-md" />
-                //             </MatDialogAction>
-                //         </MatDialog>
-                //     </section>
+                    if *is_show {
+                        <div 
+                            id="defaultModal" 
+                            class="bg-gray-900/30 fixed flex items-center justify-center top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-100 max-h-full"
+                            onclick={close_modal.clone()}
+                            >
+                            <div>
+
+                                <div class="relative w-full max-w-2xl max-h-full bg-screen ">
+                                    <div class="relative bg-screen-2nd rounded-lg shadow">
+                                        <div class="flex items-center justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                        <h3 class="font-bold text-2xl text-error">
+                                            {"本当に削除しますか？"}
+                                        </h3>
+                                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-4 h-4 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                                </svg>
+                                                <span class="sr-only">{"Close modal"}</span>
+                                            </button>
+                                        </div>
+                                        
+                                        <div class="flex items-center justify-evenly p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                            <button 
+                                                data-modal-hide="defaultModal" 
+                                                type="click" 
+                                                class="bg-white text-error border-error focus:ring-4 focus:outline-none focus:ring-error font-medium rounded-lg text-sm px-5 py-2.5 text-center" 
+                                                onclick={onclick_delete.clone()}
+                                                >{"削除する"}
+                                            </button>
+                                            <button 
+                                                data-modal-hide="defaultModal" 
+                                                type="button" 
+                                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" 
+                                                label="Decline" >
+                                                {"キャンセル"}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    }
                 </div>
             }
         }
